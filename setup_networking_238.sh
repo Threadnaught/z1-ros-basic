3d547376b62be7d1db5d694a58550553b8e64679
idk <patrickhingley@live.co.uk>
2025-07-11 16:22:43 +0100

    added config for second arm, at 238

--- setup_networking_238.sh
+++ setup_networking_238.sh
@@ -7,6 +7,9 @@ echo "Configuring $1 as interface connected to robot"
 nmcli dev set $1 managed no
 
 ip link set $1 up
-ip addr add 192.168.123.1/25 dev $1
+ip addr add 192.168.123.129/25 dev $1
 
-ping 192.168.123.110
+ping 192.168.123.238
+
+# For some DUMB reason, the arms both try to claim 8880 and it's a mess. I love UDP.
+iptables -t nat -A PREROUTING -s 192.168.1.238 -p tcp --dport 8880 -j REDIRECT --to-port 8889
